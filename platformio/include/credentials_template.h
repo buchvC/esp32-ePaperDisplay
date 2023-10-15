ae5d11a3f09efb63ed5adee59a5ca89d7398f92c
buchvC <login_cb@outlook.de>
2023-10-15 11:12:57 +0200

    add credential.h to prevent uploading credentials

---
+++ platformio/include/credentials_template.h
@@ -0,0 +1,11 @@
+/* file contains secret informations and sould not be commited to git */
+
+/* location information*/
+#define CREDENTIAL_LATITUDE                     "40.7128"
+#define CREDENTIAL_LONGITUDE                    "-74.0060"
+#define CREDENTIAL_CITY                         "New York"
+
+#define CREDENTIAL_WIFI_SSID                   "xxx"
+#define CREDENTIAL_WIFI_PASSWORD               "xxx"
+
+#define CREDENTIAL_OWM_APIKEY                  "xxx"
--- platformio/src/config.cpp
+++ platformio/src/config.cpp
@@ -17,6 +17,7 @@
 
 #include <Arduino.h>
 #include "config.h"
+#include "credentials.h"
 
 // PINS
 // ADC pin used to measure battery voltage
@@ -35,12 +36,12 @@ const uint8_t PIN_BME_SCL = 16;   // 26 for micro-usb firebeetle
 const uint8_t BME_ADDRESS = 0x76; // if sensor does not work, try 0x77
 
 // WIFI CREDENTIALS
-const char *WIFI_SSID     = "ssid";
-const char *WIFI_PASSWORD = "password";
+const char *WIFI_SSID     = CREDENTIAL_WIFI_SSID;
+const char *WIFI_PASSWORD = CREDENTIAL_WIFI_PASSWORD;
 
 // OPENWEATHERMAP API
 // OpenWeatherMap API key, https://openweathermap.org/
-const String OWM_APIKEY   = "abcdefghijklmnopqrstuvwxyz012345";
+const String OWM_APIKEY   = CREDENTIAL_OWM_APIKEY;
 const String OWM_ENDPOINT = "api.openweathermap.org";
 // OpenWeatherMap One Call 2.5 API is deprecated for all new free users
 // (accounts created after Summer 2022).
@@ -61,15 +62,15 @@ const String OWM_ONECALL_VERSION = "3.0";
 // LOCATION
 // Set your latitude and longitude.
 // (used to get weather data as part of API requests to OpenWeatherMap)
-const String LAT = "40.7128";
-const String LON = "-74.0060";
+const String LAT = CREDENTIAL_LATITUDE;
+const String LON = CREDENTIAL_LONGITUDE;
 // City name that will be shown in the top-right corner of the display.
-const String CITY_STRING = "New York";
+const String CITY_STRING = CREDENTIAL_CITY;
 
 // TIME
 // For list of time zones see
 // https://github.com/nayarsystems/posix_tz_db/blob/master/zones.csv
-const char *TIMEZONE = "CET-1CEST,M3.5.0,M10.5.0/3";
+const char *TIMEZONE = "EST5EDT,M3.2.0,M11.1.0";
 // Time format used when displaying sunrise/set times. (Max 11 characters)
 // For more information about formatting see
 // https://man7.org/linux/man-pages/man3/strftime.3.html
